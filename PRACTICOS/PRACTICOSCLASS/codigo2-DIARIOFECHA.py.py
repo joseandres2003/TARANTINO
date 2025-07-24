ea0815d5b2fbbe32a51b01465c5424bb556bb513
Alexander Yanarico <44819694-alexanderyanari@users.noreply.replit.com>
2025-07-24 15:18:44 +0000

    practicosclase

---
+++ PRACTICOS/PRACTICOSCLASS/codigo2-DIARIOFECHA.py.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+nombre_archivo = "mi_diario.txt"
+
+fecha_actual = datetime.now().strftime("%Y-%m-%d")
+
+with open(nombre_archivo, 'w') as diario_file:
+
+    diario_file.write(f"Fecha: {fecha_actual}\n\n")
+
+
+    diario_file.write("Querido diario,\n")
+    diario_file.write("Hoy aprendí sobre archivos en Python.\n")
+    diario_file.write("El modo 'w' borra todo antes de escribir. ¡Qué miedo!\n")
+
+print("¡Listo, se escribió el diario -- GRUPO TARANTINO")
